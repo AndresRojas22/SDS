7fd8eb0c0e71c05f796a2a28cd50d26e6ed9afea
Andy <andy_chapa01@hotmail.com>
2022-11-19 19:27:32 -0600

    Actualizacion usuario

--- ServicioDS/database/migrations/2014_10_12_000000_create_users_table.php
+++ ServicioDS/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('telefono');
-            $table->integer('edad');
-            $table->string('direccion');
             $table->rememberToken();
             $table->timestamps();
         });

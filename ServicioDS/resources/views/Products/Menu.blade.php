c69e1520171243f48322ac2604e11c16b476d5eb
Andy <andy_chapa01@hotmail.com>
2022-11-26 00:12:22 -0600

    Correcioin

--- ServicioDS/resources/views/Products/Menu.blade.php
+++ ServicioDS/resources/views/Products/Menu.blade.php
@@ -28,13 +28,14 @@
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Iniciar sesion
+            Bienvenido {{auth()->user()->name}}
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Iniciar sesion</a></li>
+            <li><a class="dropdown-item" href="#">{{auth()->user()->name}}</a></li>
             <li><a class="dropdown-item" href="#">Mis pedidos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Crear cuenta</a></li>
+            <li><a class="dropdown-item" href="">Crear cuenta</a></li>
+            <li><a class="dropdown-item" href="{{route('logout')}}" id="logout">Cerrar sesion</a></li>
           </ul>
         </li>
           <li class="nav-item">
--- ServicioDS/routes/web.php
+++ ServicioDS/routes/web.php
@@ -1,20 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request;
+use App\Products;
 
 
-
+Route::middleware('auth')->group(function(){
     Route::get('/products',function(){
         return view('products.menu');
     })->name('products.menu');
@@ -23,11 +14,6 @@ use Illuminate\Support\Facades\Route;
  Route::get('/storage', function () {
     return view('admin.storage');
 });
+});
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

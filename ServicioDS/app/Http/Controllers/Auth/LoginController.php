ec2ee6439c2b9bd3c3316908b6c3ac526e089ef6
Andy <andy_chapa01@hotmail.com>
2022-11-26 01:55:07 -0600

    Login Admin-user

--- ServicioDS/app/Http/Controllers/Auth/LoginController.php
+++ ServicioDS/app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -46,5 +47,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectPath()
+    {
+        if(Auth::user()->tipo_usuario){
+            return 'Admin.Almacen';
+        }
+        return '/products';
     }
 }
